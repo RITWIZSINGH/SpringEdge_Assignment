1fbb7431f8e3d80c854d7847dfadd7f9ace7c4ff
Ritwiz <madangopalsingh2803@gmail.com>
2024-09-08 22:14:33 +0530

    Created a file for demologinpage

---
+++ lib/demologinpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoLoginPage extends StatefulWidget {
+  const DemoLoginPage({super.key});
+
+  @override
+  State<DemoLoginPage> createState() => _DemoLoginPageState();
+}
+
+class _DemoLoginPageState extends State<DemoLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
